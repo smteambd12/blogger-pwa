405f587d9bc1c9c70b64d572cf0a2eca93aae542
smteambd12 <smteambdofficial@gmail.com>
2024-09-07 15:33:40 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -28,9 +28,9 @@ const APP_CONFIG = {
     name: 'fineshop-pwa-app'
   },
   github: {
-    username: '<username>',
-    repository: '<repository>',
-    branch: '<branch>',
+    username: 'smteambd12',
+    repository: 'blogger-pwa',
+    branch: 'main',
     /**
      * If the repository is private, provide token as string
      * otherwise keep it null
